9e2f6b5987aa9540cf645f4d82077f3bb697cd85
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-01-02 22:38:36 -0500

    things

--- mpi/rusty/run.sh
+++ mpi/rusty/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH -J run
-#SBATCH -o run.o%j
-#SBATCH -e run.e%j
-#SBATCH -N 8
+#SBATCH -J apogee
+#SBATCH -o apogee.o%j
+#SBATCH -e apogee.e%j
+#SBATCH -N 4
 #SBATCH -t 24:00:00
-#SBATCH -p cca
+#SBATCH -p gen
 
 source ~/.bash_profile
 
 cd /mnt/ceph/users/apricewhelan/projects/hq/scripts
 
-module load gcc openmpi2
+module load gcc openmpi2 lib/hdf5/1.10.1
 
 conda activate hq
 
 date
 
-srun python run_apogee.py -v -c ../run_config/apogee.yml --mpi
+mpirun python run_apogee.py -v -c ../run_config/apogee.yml --mpi
 
 date
