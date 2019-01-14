13038cf23144d5d08c0d0ddfea9c0e4ef088653d
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-01-14 15:31:57 -0500

    fix load submit

--- mpi/rusty/load.sh
+++ mpi/rusty/load.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#SBATCH -J load
-#SBATCH -o load.o%j
-#SBATCH -e load.e%j
+#SBATCH -J apogee-load
+#SBATCH -o apogee-load.o%j
+#SBATCH -e apogee-load.e%j
 #SBATCH -n 1
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 #SBATCH -p cca
 
 source ~/.bash_profile
 
 cd /mnt/ceph/users/apricewhelan/projects/hq/scripts
 
+module load gcc openmpi2 lib/hdf5/1.10.1
+
 conda activate hq
 
 date
 
-python init_db.py --allstar=../data/allStar-t9-l31c-58247.fits --allvisit=../data/allVisit-t9-l31c-58247.fits -v
+python db_init.py --allstar=../data/allStar-t9-l31c-58247.fits --allvisit=../data/allVisit-t9-l31c-58247.fits -v
 
 date
