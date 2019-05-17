e891722028e28c3971f7cd98e4bae6c7660107ea
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-05-17 19:20:30 -0400

    remove database shit

--- hq/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from .connect import db_connect
-from .model import *
-from .init import initialize_db
-from .helpers import *
--- hq/db/tests/setup_package.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-def get_package_data():
-    return {'hq.db.tests': ['*.yml', '*.fits']}
