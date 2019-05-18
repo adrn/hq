c9c45118a148fbd1ac4c8c01096df42bb57d1a0b
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-05-17 20:30:03 -0400

    script to generate prior cache

---
+++ scripts/make_prior_cache.py
@@ -0,0 +1,57 @@
+# Standard library
+from os import path
+
+# Third-party
+import astropy.units as u
+import yaml
+from thejoker import TheJoker
+
+# Project
+from hq.config import HQ_CACHE_PATH, config_to_jokerparams
+from hq.log import logger
+from hq.script_helpers import get_parser
+from hq.sample_prior import make_prior_cache
+
+
+def main(name, overwrite):
+    run_path = path.join(HQ_CACHE_PATH, name)
+
+    with open(path.join(run_path, 'config.yml'), 'r') as f:
+        config = yaml.load(f.read())
+
+    params = config_to_jokerparams(config)
+    prior_filename = ('P{0:.0f}-{1:.0f}_{2:d}_prior_samples.hdf5'
+                      .format(params.P_min.to_value(u.day),
+                              params.P_max.to_value(u.day),
+                              config['prior']['n_samples']))
+    prior_cache_path = path.join(HQ_CACHE_PATH, prior_filename)
+
+    if path.exists(prior_cache_path) and not overwrite:
+        logger.debug("Prior cache file already exists at '{}'! User -o or "
+                     "--overwrite to re-generate.".format(prior_cache_path))
+        return
+
+    n_samples = config['prior']['n_samples']
+    logger.debug("Prior samples file not found - generating {} samples in "
+                 "cache file at {}...".format(n_samples, prior_cache_path))
+    make_prior_cache(prior_cache_path, TheJoker(params), nsamples=n_samples)
+    logger.debug("...done generating cache.")
+
+
+if __name__ == "__main__":
+    # Define parser object
+    parser = get_parser(description='Generate an HQ run template with the '
+                                    'specified name. After running this, you '
+                                    'then have to go in to the run directory '
+                                    'and edit the configuration.',
+                        loggers=logger)
+
+    parser.add_argument("--name", dest="run_name", required=True,
+                        type=str, help="The name of the run.")
+    parser.add_argument("-o", "--overwrite", dest="overwrite", default=False,
+                        action="store_true",
+                        help="Overwrite any existing results for this run.")
+
+    args = parser.parse_args()
+
+    main(args.run_name, args.overwrite)
