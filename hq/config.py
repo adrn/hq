1af41c0b01a77af68d6a4c1fedcc9ce8712b0c7b
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-05-19 12:02:08 -0400

    move function to read alldata

--- hq/config.py
+++ hq/config.py
@@ -2,6 +2,8 @@ import os
 from os.path import expanduser, join
 
 # Third-party
+from astropy.io import fits
+from astropy.table import Table
 import astropy.units as u
 import numpy as np
 
@@ -56,3 +58,56 @@ def config_to_prior_cache(config, params):
                               params.P_max.to_value(u.day),
                               config['prior']['n_samples']))
     return join(HQ_CACHE_PATH, prior_filename)
+
+
+def config_to_alldata(config):
+    allstar_tbl = fits.getdata(config['data']['allstar'])
+    allvisit_tbl = fits.getdata(config['data']['allvisit'])
+
+    # Remove bad velocities / NaN / Inf values:
+    allvisit_tbl = allvisit_tbl[np.isfinite(allvisit_tbl['VHELIO']) &
+                                np.isfinite(allvisit_tbl['VRELERR'])]
+    allvisit_tbl = allvisit_tbl[(allvisit_tbl['VRELERR'] < 100.) &
+                                (allvisit_tbl['VHELIO'] != -9999)]
+
+    starflag_bits = config['data'].get('starflag_bits', None)
+    aspcapflag_bits = config['data'].get('aspcapflag_bits', None)
+    min_nvisits = config['data'].get('min_nvisits', 3)
+
+    if starflag_bits is None: # use deaults
+        # LOW_SNR, PERSIST_HIGH, PERSIST_JUMP_POS, PERSIST_JUMP_NEG
+        starflag_bits = [4, 9, 12, 13]
+
+        # VERY_BRIGHT_NEIGHBOR, SUSPECT_RV_COMBINATION, SUSPECT_BROAD_LINES
+        starflag_bits += [3, 16, 17]
+
+    starflag_mask = np.sum(2 ** np.array(starflag_bits))
+    allvisit_tbl = allvisit_tbl[(allvisit_tbl['STARFLAG'] & starflag_mask) == 0]
+
+    # After quality and bitmask cut, figure out what APOGEE_IDs remain
+    v_apogee_ids, counts = np.unique(allvisit_tbl['APOGEE_ID'],
+                                     return_counts=True)
+    star_mask = np.isin(allstar_tbl['APOGEE_ID'],
+                        v_apogee_ids[counts >= min_nvisits])
+
+    if aspcapflag_bits is None: # use defaults
+        # TEFF_WARN, ROTATION_WARN, CHI2_WARN, STAR_BAD
+        aspcapflag_bits = [0, 8, 10, 23]
+    skip_mask = np.sum(2 ** np.array(aspcapflag_bits))
+    star_mask &= ((allstar_tbl['ASPCAPFLAG'] & skip_mask) == 0)
+
+    # Remove stars flagged with:
+    # VERY_BRIGHT_NEIGHBOR, SUSPECT_RV_COMBINATION, SUSPECT_BROAD_LINES
+    star_mask &= ((allstar_tbl['STARFLAG'] & starflag_mask) == 0)
+    allstar_tbl = allstar_tbl[star_mask]
+
+    # Only load visits for stars that we're loading
+    allvisit_tbl = allvisit_tbl[np.isin(allvisit_tbl['APOGEE_ID'],
+                                        allstar_tbl['APOGEE_ID'])]
+    v_apogee_ids2 = np.unique(allvisit_tbl['APOGEE_ID'])
+    star_mask2 = np.isin(allstar_tbl['APOGEE_ID'], v_apogee_ids2)
+
+    allvisit_tbl = Table(allvisit_tbl)
+    allstar_tbl = Table(allstar_tbl[star_mask2])
+
+    return allstar_tbl, allvisit_tbl
