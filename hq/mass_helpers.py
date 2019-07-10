ca666940750d11cf8a05ef529677fd92de05773c
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-07-10 14:23:26 +0200

    function to compute masses

--- hq/mass_helpers.py
+++ hq/mass_helpers.py
@@ -37,6 +37,21 @@ def CM_NM_to_CNM(M_H, C_M, N_M):
     return CN_H - M_H
 
 
+def get_martig_mask(data):
+    # Teff, logg, vmicro, M_H, C_M, N_M
+    fparam = data['FPARAM']
+
+    CN = fparam[:, 4] - fparam[:, 5]
+    mask = ((fparam[:, 3] > -0.8) &
+            (fparam[:, 0] > 4000) & (fparam[:, 0] < 5000) &
+            (fparam[:, 1] > 1.8) & (fparam[:, 1] < 3.3) &
+            (fparam[:, 4] > -0.25) & (fparam[:, 4] < 0.15) &
+            (fparam[:, 5] > -0.1) & (fparam[:, 5] < 0.45) &
+            (CN > -0.6) & (CN < 0.2))
+
+    return mask
+
+
 def get_martig_vec(Teff, logg, M_H, C_M, N_M):
     """Produces a 1D vector that can be inner-producted with the upper-triangle
     matrix provided by Martig et al. to estimate the stellar mass.
@@ -56,3 +71,17 @@ def get_martig_vec(Teff, logg, M_H, C_M, N_M):
     vec[6] = logg
 
     return vec.T
+
+
+def get_martig_mass(allstar):
+    ms = np.full(np.nan, len(allstar))
+    mask = get_martig_mask(allstar)
+
+    # subset of allStar rows that are within the martig selection
+    martig_t = allstar[mask]
+    vec = get_martig_vec(martig_t['FPARAM'][:, 0], martig_t['FPARAM'][:, 1],
+                         martig_t['FPARAM'][:, 3], martig_t['FPARAM'][:, 4],
+                         martig_t['FPARAM'][:, 5])
+    ms[mask] = np.einsum('ij,ni,nj->n', M, vec, vec)
+
+    return ms
