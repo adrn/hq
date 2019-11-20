b32c2943cf39e8a71449f5306339447a0c3a8e50
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-11-19 21:19:20 -0500

    update test

--- hq/tests/test_sample_prior.py
+++ hq/tests/test_sample_prior.py
@@ -1,3 +1,5 @@
+import logging
+
 # Third-party
 import astropy.units as u
 import h5py
@@ -5,6 +7,7 @@ import thejoker as tj
 
 # Project
 from ..sample_prior import make_prior_cache
+from ..log import logger
 
 
 def test_make_prior_cache(tmpdir):
@@ -15,7 +18,10 @@ def test_make_prior_cache(tmpdir):
         P_min=8*u.day, P_max=8192*u.day,
         sigma_K0=30*u.km/u.s, sigma_v=100*u.km/u.s)
 
-    make_prior_cache(filename, prior, nsamples=N, batch_size=2**14)
+    old_level = logger.getEffectiveLevel()
+    logger.setLevel(logging.ERROR)
+    make_prior_cache(filename, prior, n_total_samples=N, batch_size=2**16)
+    logger.setLevel(old_level)
 
     with h5py.File(filename, 'r') as f:
-        assert f['samples'].shape == (N, 5)
+        assert f['samples'].shape == (N, )
