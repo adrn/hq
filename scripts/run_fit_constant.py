73eb2ffca77d05d34c5e40e06b3f4b185e16e4ec
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-11-28 09:47:42 -0500

    new fit constant

--- scripts/run_fit_constant.py
+++ scripts/run_fit_constant.py
@@ -10,7 +10,8 @@ import numpy as np
 from thejoker.logging import logger as joker_logger
 from tqdm import tqdm
 from scipy.optimize import minimize
-from thejoker.data import RVData
+import thejoker as tj
+from thejoker.multiproc_helpers import batch_tasks
 
 # Project
 from hq.log import logger
@@ -35,28 +36,44 @@ def neg_ln_likelihood(*args, **kwargs):
 
 
 def worker(task):
-    apogee_id, data = task
+    apogee_ids, worker_id, c = task
 
-    rv = data.rv.to_value(u.km/u.s).astype('f8')
-    var = data.rv_err.to_value(u.km/u.s).astype('f8') ** 2
+    logger.debug(f"Worker {worker_id} preparing to run {len(apogee_ids)} stars")
 
-    # First optimize the negative log-likelihood for the robust constant model:
-    res = minimize(neg_ln_likelihood,
-                   x0=(np.mean(rv.astype('f8')), -4, 0.75),
-                   args=(rv, var), method='L-BFGS-B',
-                   bounds=[(-500, 500), (-10, 8), (0.5, 1.)],
-                   options=dict(ftol=1e-11))
-    ll_robust = -res.fun
+    rows = []
+    for apogee_id in apogee_ids:
+        with h5py.File(c.tasks_path, 'r') as tasks_f:
+            data = tj.RVData.from_timeseries(tasks_f[apogee_id])
 
-    opt_mu = np.sum(rv/var) / np.sum(1/var)
-    ll_basic = ln_likelihood_robust([opt_mu, -8, 1], rv, var)
+        rv = data.rv.to_value(u.km/u.s).astype('f8')
+        var = data.rv_err.to_value(u.km/u.s).astype('f8') ** 2
 
-    resp = dict()
-    resp['APOGEE_ID'] = apogee_id
-    resp['robust_constant_ln_likelihood'] = ll_robust
-    resp['robust_success'] = res.success
-    resp['constant_ln_likelihood'] = ll_basic
-    return resp
+        # Optimize the negative log-likelihood for the robust constant model:
+        try:
+            res = minimize(neg_ln_likelihood,
+                           x0=(np.mean(rv.astype('f8')), -4, 0.75),
+                           args=(rv, var), method='L-BFGS-B',
+                           bounds=[(-500, 500), (-10, 8), (0.5, 1.)],
+                           options=dict(ftol=1e-11))
+            ll_robust = -res.fun
+
+            opt_mu = np.sum(rv/var) / np.sum(1/var)
+            ll_basic = ln_likelihood_robust([opt_mu, -8, 1], rv, var)
+        except Exception as e:
+            logger.error(f"FAILED ON {apogee_id}")
+            print(e)
+            continue
+
+        resp = dict()
+        resp['APOGEE_ID'] = apogee_id
+        resp['robust_constant_ln_likelihood'] = ll_robust
+        resp['robust_success'] = res.success
+        resp['constant_ln_likelihood'] = ll_basic
+        rows.append(resp)
+
+    logger.debug(f"Worker {worker_id} finished batch")
+
+    return rows
 
 
 def main(run_name, pool, overwrite=False, seed=None):
@@ -74,20 +91,25 @@ def main(run_name, pool, overwrite=False, seed=None):
         raise IOError("Tasks file '{0}' does not exist! Did you run "
                       "make_tasks.py?")
 
-    tasks = []
-    with h5py.File(c.tasks_path, 'r') as tasks_f:
-        for apogee_id in tasks_f:
-            data = RVData.from_timeseries(tasks_f[apogee_id])
-            tasks.append((apogee_id, data))
+    # Get data files out of config file:
+    logger.debug("Loading data...")
+    allstar, _ = c.load_alldata()
+    apogee_ids = np.unique(allstar['APOGEE_ID'])
 
-    logger.info('Done preparing tasks: {0} stars in process queue'
-                .format(len(tasks)))
+    # Make batches of APOGEE_IDs
+    logger.debug("Preparing tasks...")
+    if len(apogee_ids) > 10 * pool.size:
+        n_tasks = min(16 * pool.size, len(apogee_ids))
+    else:
+        n_tasks = pool.size
+    tasks = batch_tasks(len(apogee_ids), n_tasks, arr=apogee_ids, args=(c, ))
+    logger.info(f'Done preparing tasks: split into {len(tasks)} task chunks')
 
     results = []
     for r in tqdm(pool.map(worker, tasks), total=len(tasks)):
         results.append(r)
 
-    tbl = Table(results)
+    tbl = Table([item for sublist in results for item in sublist])
     tbl.write(results_path, overwrite=True)
 
 
@@ -106,6 +128,6 @@ if __name__ == '__main__':
     with threadpool_limits(limits=1, user_api='blas'):
         with args.Pool(**args.Pool_kwargs) as pool:
             main(run_name=args.run_name, pool=pool, overwrite=args.overwrite,
-                seed=args.seed)
+                 seed=args.seed)
 
     sys.exit(0)
