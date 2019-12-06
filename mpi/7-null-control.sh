49d17eb6a0180e2d58d64cdb88236a1fa326c80c
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-12-06 10:31:57 -0500

    null control sample

--- mpi/7-null-control.sh
+++ mpi/7-null-control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J apogee-control
-#SBATCH -o apogee-control.o%j
-#SBATCH -e apogee-control.e%j
+#SBATCH -o logs/null-control.o%j
+#SBATCH -e logs/null-control.e%j
 #SBATCH -n 720
 #SBATCH -t 72:00:00
 #SBATCH -p cca
@@ -9,17 +9,16 @@
 
 source ~/.bash_profile
 init_conda
+conda activate hq2
 echo $HQ_RUN
 
 cd /mnt/ceph/users/apricewhelan/projects/hq/scripts
 
-module load gcc openmpi2 lib/hdf5/1.10.1 intel/mkl/2019-3 slurm
-
-conda activate hq
 
 date
 
-stdbuf -o0 -e0 mpirun -n $SLURM_NTASKS python -m mpi4py.futures run_null_control.py --name $HQ_RUN -v --mpi
+# stdbuf -o0 -e0 mpirun -n $SLURM_NTASKS python -m mpi4py.futures run_null_control.py --name $HQ_RUN -v --mpi
+mpirun -n $SLURM_NTASKS python3 run_null_control.py --name $HQ_RUN --mpi -v
 
 date
 
