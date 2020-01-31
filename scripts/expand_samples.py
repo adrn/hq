1c0fcb45abeec6f3724a4c1d4b2d1ee36e388659
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-01-31 16:32:43 -0500

    expand samples script

--- scripts/expand_samples.py
+++ scripts/expand_samples.py
@@ -8,109 +8,79 @@ import numpy as np
 import h5py
 from tqdm import tqdm
 import yaml
-from thejoker import JokerSamples
-from schwimmbad import SerialPool
-from schwimmbad.mpi import MPIAsyncPool
+import thejoker as tj
 
 # Project
 from hq.log import logger
-from hq.config import HQ_CACHE_PATH, config_to_alldata
-from hq.script_helpers import get_parser
+from hq.config import Config
 
 
 def worker(apogee_id, results_path, output_path, config):
     with h5py.File(results_path, 'r') as f:
-        samples = JokerSamples.from_hdf5(
-            f[apogee_id], poly_trend=config['hyperparams']['poly_trend'])
+        samples = tj.JokerSamples.read(f[apogee_id])
 
-        more_cols = dict()
-        if ('ln_likelihood' in f[apogee_id].keys() and
-                f[apogee_id + '/ln_likelihood'].shape != ()):
-            more_cols['ln_likelihood'] = f[apogee_id]['ln_likelihood'][:]
-            more_cols['ln_prior'] = f[apogee_id]['ln_prior'][:]
-        else:
-            more_cols['ln_likelihood'] = np.full(len(samples), np.nan)
-            more_cols['ln_prior'] = np.full(len(samples), np.nan)
+        # more_cols = dict()
+        # if ('ln_likelihood' in f[apogee_id].keys() and
+        #         f[apogee_id + '/ln_likelihood'].shape != ()):
+        #     more_cols['ln_likelihood'] = f[apogee_id]['ln_likelihood'][:]
+        #     more_cols['ln_prior'] = f[apogee_id]['ln_prior'][:]
+        # else:
+        #     more_cols['ln_likelihood'] = np.full(len(samples), np.nan)
+        #     more_cols['ln_prior'] = np.full(len(samples), np.nan)
 
     res = samples.to_table()
-    for k in more_cols:
-        res[k] = more_cols[k]
-
-    if 'jitter' in config['hyperparams']:
-        res.remove_column('jitter')
+    # for k in more_cols:
+    #     res[k] = more_cols[k]
 
     res.write(join(output_path, apogee_id[:4], '{}.fits.gz'.format(apogee_id)),
               overwrite=True)
 
 
 def main(run_name, pool, overwrite=False):
-    run_path = join(HQ_CACHE_PATH, run_name)
-    with open(join(run_path, 'config.yml'), 'r') as f:
-        config = yaml.load(f.read())
+    c = Config.from_run_name(run_name)
+    allstar, allvisit = c.load_alldata()
 
-    allstar, allvisit = config_to_alldata(config)
-
-    samples_path = join(HQ_CACHE_PATH, run_name, 'samples')
+    samples_path = join(c.run_path, 'samples')
     logger.debug(f'Writing samples to {samples_path}')
 
     unq_stubs = np.unique([x[:4] for x in allstar['APOGEE_ID']])
     for stub in unq_stubs:
         os.makedirs(join(samples_path, stub), exist_ok=True)
 
-    joker_results_path = join(HQ_CACHE_PATH, run_name,
-                              'thejoker-samples.hdf5')
-    emcee_results_path = join(HQ_CACHE_PATH, run_name,
-                              'emcee-samples.hdf5')
-
     tasks = []
 
-    joker_f = h5py.File(joker_results_path, 'r')
-    emcee_f = h5py.File(emcee_results_path, 'r')
+    joker_f = h5py.File(c.joker_results_path, 'r')
+    mcmc_f = h5py.File(c.mcmc_results_path, 'r')
 
     logger.debug('Reading {} APOGEE IDs from Joker samplings'
                  .format(len(joker_f.keys())))
 
     for apogee_id in joker_f.keys():
-        if apogee_id in emcee_f.keys():
-            tasks.append((apogee_id, emcee_results_path,
-                          samples_path, config))
+        if apogee_id in mcmc_f.keys():
+            tasks.append((apogee_id, c.mcmc_results_path,
+                          samples_path, c))
         else:
-            tasks.append((apogee_id, joker_results_path,
-                          samples_path, config))
+            tasks.append((apogee_id, c.joker_results_path,
+                          samples_path, c))
 
     joker_f.close()
-    emcee_f.close()
+    mcmc_f.close()
 
     for r in tqdm(pool.starmap(worker, tasks), total=len(tasks)):
         pass
 
 
 if __name__ == '__main__':
+    from threadpoolctl import threadpool_limits
+    from hq.script_helpers import get_parser
+
     # Define parser object
-    parser = get_parser(description='TODO',
-                        loggers=logger)
-
-    parser.add_argument("--name", dest="run_name", required=True,
-                        type=str, help="The name of the run.")
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--procs", dest="n_procs", default=1,
-                       type=int, help="Number of processes.")
-    group.add_argument("--mpi", dest="mpi", default=False,
-                       action="store_true", help="Run with MPI.")
-
-    parser.add_argument("-o", "--overwrite", dest="overwrite", default=False,
-                        action="store_true",
-                        help="Overwrite any existing samplings")
+    parser = get_parser(description='TODO', loggers=[])
 
     args = parser.parse_args()
 
-    if args.mpi:
-        Pool = MPIAsyncPool
-    else:
-        Pool = SerialPool
-
-    with Pool() as pool:
-        main(run_name=args.run_name, pool=pool)
+    with threadpool_limits(limits=1, user_api='blas'):
+        with args.Pool(**args.Pool_kwargs) as pool:
+            main(run_name=args.run_name, pool=pool, overwrite=args.overwrite)
 
     sys.exit(0)
