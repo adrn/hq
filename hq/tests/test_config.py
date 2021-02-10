42b0f46dcb5b4c8402b8efdbbf040b3dc77e9556
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-10 16:06:05 -0500

    test loading hte prior

--- hq/tests/test_config.py
+++ hq/tests/test_config.py
@@ -8,7 +8,6 @@ from ..config import Config
 @pytest.mark.usefixtures("make_config")
 def test_config(make_config):
     config_file = make_config
-
     c = Config(config_file)
 
     assert c.name == 'hqtest'
@@ -17,3 +16,13 @@ def test_config(make_config):
 
     assert c.source_id_colname == 'APOGEE_ID'
     assert c.max_prior_samples == 1_000_000
+
+
+@pytest.mark.usefixtures("make_config")
+def test_load_prior(make_config):
+    config_file = make_config
+    c = Config(config_file)
+
+    prior = c.get_prior()
+    prior_samples = prior.sample(size=100)
+    assert len(prior_samples['P']) == 100
