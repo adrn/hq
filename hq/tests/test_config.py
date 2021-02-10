697da06d639189d96892d875cc359aed124bbf29
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-10 13:01:41 -0500

    actually check some values against the file

--- hq/tests/test_config.py
+++ hq/tests/test_config.py
@@ -28,5 +28,10 @@ def test_config(make_config):
     config_file = make_config
 
     c = Config(config_file)
-    print(c)
-    pass
+
+    assert c.name == 'hqtest'
+    assert c.input_data_file.exists()
+    assert c.input_data_format is None
+
+    assert c.source_id_colname == 'APOGEE_ID'
+    assert c.max_prior_samples == 1_000_000
