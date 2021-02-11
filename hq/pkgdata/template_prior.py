08b3ed081791d87ae4b68d5bffd20f34ea53456c
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-11 17:08:16 -0500

    update template prior to have an mcmc section

--- hq/pkgdata/template_prior.py
+++ hq/pkgdata/template_prior.py
@@ -2,13 +2,30 @@
 import astropy.units as u
 import pymc3 as pm
 import exoplanet.units as xu
+from exoplanet.distributions import Angle
 import thejoker as tj
 
-# The prior used to run The Joker: please edit below
+# The prior used to run The Joker: please edit below! Any parameters set to None
+# should be changed to real values!
 with pm.Model() as model:
     # Set up the default Joker prior:
     prior = tj.JokerPrior.default(
         P_min=None, P_max=None,
         sigma_K0=None,
         sigma_v=None
+    )
+
+with pm.Model() as model:
+    # See note above: when running MCMC, we will sample in the parameters
+    # (M0 - omega, omega) instead of (M0, omega)
+    M0_m_omega = xu.with_unit(Angle('M0_m_omega'), u.radian)
+    omega = xu.with_unit(Angle('omega'), u.radian)
+    M0 = xu.with_unit(pm.Deterministic('M0', M0_m_omega + omega),
+                      u.radian)
+
+    prior_mcmc = tj.JokerPrior.default(
+        P_min=None, P_max=None,
+        sigma_K0=None,
+        sigma_v=None,
+        pars={'M0': M0, 'omega': omega}
     )
