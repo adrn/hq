88aebc3759e010beeb5b20b11dd2da3066ebfc2e
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-12 14:32:55 -0500

    disable favicon

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,7 @@ html_sidebars = {}
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = ''
+html_favicon = ''
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
