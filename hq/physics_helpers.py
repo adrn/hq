c4fb8caac4f0241995b390bde952deb62494f136
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-12 23:09:48 -0500

    m2min

--- hq/physics_helpers.py
+++ hq/physics_helpers.py
@@ -12,3 +12,18 @@ def period_at_surface(M1, logg, e, M2=0*u.Msun):
     q = M2 / M1
     P = 2*np.pi * (G*(M1+M2) / R1**3)**(-1/2) * (1-e)**(-3/2) * (1+q)**1.5
     return P.to(u.day)
+
+
+def fast_mf(P, K, e):
+    """Binary mass function."""
+    mf_circ = P * K**3 / (2*np.pi * G)
+    return mf_circ.to(u.Msun) * (1 - e**2)**1.5
+
+
+def fast_m2_min(m1, mf):
+    return (2*mf + np.power(2,2/3)*
+      np.power(mf*(27*pow(m1,2) + 18*m1*mf + 2*np.power(mf,2) +
+          3*np.sqrt(3)*np.sqrt(np.power(m1,3)*(27*m1 + 4*mf))),1/3) +
+     (2*mf*(6*m1 + mf))/
+      np.power((27*np.power(m1,2)*mf)/2. + 9*m1*np.power(mf,2) + np.power(mf,3) +
+        (3*np.sqrt(3)*mf*np.sqrt(np.power(m1,3)*(27*m1 + 4*mf)))/2.,1/3))/6.
