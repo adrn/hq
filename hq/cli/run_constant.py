d4e78614b80ceddadc36f1537922dc15fc09adfc
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-02-28 10:19:54 -0500

    fix robust constant model and add robust linear model

--- hq/cli/run_constant.py
+++ hq/cli/run_constant.py
@@ -22,29 +22,38 @@ from hq.log import logger
 from hq.config import Config
 
 
-def get_robust_constant_model():
-    with pm.Model() as model:
-        rv = pm.Data('rv', np.zeros(1))
-        rv_err = pm.Data('rv_err', np.zeros(1))
-
-        # cluster sizes - prior has some MAGIC NUMBERz
-        w = pm.Dirichlet("w", a=np.array([10, 1.]), shape=2)
+def get_robust_constant_model(data):
+    with pm.Model() as const_model:
+        rv = pm.Data('rv', data['rv'])
+        rv_err = pm.Data('rv_err', data['rv_err'])
 
         mu = pm.Uniform('mu', -500, 500)
         lns = pm.Uniform('lns', -8, 6)
-        err2 = tt.sqrt(rv_err**2 + tt.exp(2 * lns))
+        err = tt.sqrt(rv_err**2 + tt.exp(2 * lns))
 
-        mean = tt.stack((mu * tt.ones(rv.shape),
-                        mu * tt.ones(rv.shape))).T
-        sigma = tt.stack((rv_err, err2)).T
-
-        like = pm.NormalMixture(
-            'like', w=w, mu=mean, sigma=sigma,
-            observed=rv)
-
+        like = pm.Normal('like', mu, err,
+                         observed=rv)
         pm.Deterministic('loglike', like.logpt)
 
-    return model
+    return const_model
+
+
+def get_robust_linear_model(data):
+    with pm.Model() as linear_model:
+        t = pm.Data('t', data['t'])
+        rv = pm.Data('rv', data['rv'])
+        rv_err = pm.Data('rv_err', data['rv_err'])
+
+        a = pm.Uniform('a', -500, 500)
+        b = pm.Uniform('b', -500, 500)
+        lns = pm.Uniform('lns', -8, 6)
+        err = tt.sqrt(rv_err**2 + tt.exp(2 * lns))
+
+        like = pm.Normal('like', a + b*t, err,
+                         observed=rv)
+        pm.Deterministic('loglike', like.logpt)
+
+    return linear_model
 
 
 def worker(task):
@@ -52,7 +61,8 @@ def worker(task):
 
     logger.debug(f"Worker {worker_id} running {len(source_ids)} stars")
 
-    model = get_robust_constant_model()
+    const_model = get_robust_constant_model()
+    linear_model = get_robust_linear_model()
 
     all_data = {}
     with h5py.File(c.tasks_file, 'r') as tasks_f:
@@ -64,34 +74,75 @@ def worker(task):
     for source_id in source_ids:
         data = all_data[source_id]
 
+        dt = (data.t.mjd - np.mean(data.t.mjd)) / 1000.
         rv = data.rv.to_value(u.km/u.s).astype('f8')
         rv_err = data.rv_err.to_value(u.km/u.s).astype('f8')
-        data_dict = {'rv': rv, 'rv_err': rv_err}
+        data_dict = {'t': dt, 'rv': rv, 'rv_err': rv_err}
+
+        init_s = np.sqrt(np.sum((data.rv - np.mean(data.rv))**2 -
+                                data.rv_err**2) / len(data))
+        init_lns = np.log(init_s.value)
 
         # Find the MAP of the robust constant model:
         try:
-            with model:
-                pm.set_data(data_dict)
-                map_estimate, info = pmx.optimize(
+            with const_model:
+                const_map_p, info = pmx.optimize(
                     start={'mu': np.mean(rv),
-                           'lns': -4.,
-                           'w': np.array([0.95, 0.05])},
+                           'lns': init_lns},
                     method='L-BFGS-B',
                     progress=False, verbose=False, return_info=True)
-
-            # Non-robust / standard constant log-likelihood
-            eval_mu = np.sum(rv / rv_err**2) / np.sum(1. / rv_err**2)
-            constant_ll = norm.logpdf(rv, eval_mu, rv_err).sum()
         except Exception as e:
-            logger.error(f"FAILED for source: {source_id}")
+            logger.error(f"Const FAILED for source: {source_id}")
             print(e)
-            continue
+            const_map_p = None
+
+        try:
+            with linear_model:
+                linear_map_p, info = pmx.optimize(
+                    start={'a': np.mean(rv),
+                           'b': np.polyfit(data_dict['t'], data_dict['rv'],
+                                           deg=1)[0],
+                           'lns': init_lns},
+                    method='L-BFGS-B',
+                    progress=False, verbose=False, return_info=True)
+        except Exception as e:
+            logger.error(f"Linear FAILED for source: {source_id}")
+            print(e)
+            linear_map_p = None
+
+        # Non-robust / standard constant log-likelihood
+        eval_mu = np.sum(rv / rv_err**2) / np.sum(1. / rv_err**2)
+        constant_ll = norm.logpdf(rv, eval_mu, rv_err).sum()
 
         resp = dict()
         resp[c.source_id_colname] = source_id
-        resp['robust_constant_ln_likelihood'] = map_estimate['loglike']
-        resp['robust_success'] = info.success
+
         resp['constant_ln_likelihood'] = constant_ll
+
+        if const_map_p is not None:
+            resp['robust_constant_mean'] = const_map_p['mu']
+            resp['robust_constant_scatter'] = np.exp(const_map_p['lns'])
+            resp['robust_constant_ln_likelihood'] = const_map_p['loglike']
+            resp['robust_constant_success'] = True
+        else:
+            resp['robust_constant_mean'] = np.nan
+            resp['robust_constant_scatter'] = np.nan
+            resp['robust_constant_ln_likelihood'] = np.nan
+            resp['robust_constant_success'] = False
+
+        if linear_map_p is not None:
+            resp['robust_linear_a'] = linear_map_p['a']
+            resp['robust_linear_b'] = linear_map_p['b']
+            resp['robust_linear_scatter'] = np.exp(linear_map_p['lns'])
+            resp['robust_linear_ln_likelihood'] = linear_map_p['loglike']
+            resp['robust_linear_success'] = True
+        else:
+            resp['robust_linear_a'] = np.nan
+            resp['robust_linear_b'] = np.nan
+            resp['robust_linear_scatter'] = np.nan
+            resp['robust_linear_ln_likelihood'] = np.nan
+            resp['robust_linear_success'] = False
+
         rows.append(resp)
 
     logger.debug(f"Worker {worker_id} finished batch")
