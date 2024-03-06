0b89e8537dae984621427b1eb340eb350f121063
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-03-06 16:48:57 -0500

    swap setup.cfg for pyproject

--- hq/__init__.py
+++ hq/__init__.py
@@ -1,11 +1,1 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    pass
+from ._version import version as __version__  # noqa
